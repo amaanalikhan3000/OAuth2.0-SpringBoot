ba4e339c36492c91a484bd0b66650dfa1515aca0
amaanalikhan3000 <amaanali.amaanali@gmail.com>
2024-09-11 06:59:52 +0530

    Initial commit and added files

---
+++ src/main/java/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/*
+   This SecurityConfiguration Class Disables CSRF protection & Requires authentication for
+    every request to the app. Enables OAuth2 login to allow users to authenticate using
+    third-party providers like Google or GitHub.
+ */
+
+
+@Configuration
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .authorizeHttpRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2Login();
+
+        return http.build();
+
+    }
+
+
+}
---
+++ src/test/java/com/aak/oauth2/social2/Social2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aak.oauth2.social2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Social2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
